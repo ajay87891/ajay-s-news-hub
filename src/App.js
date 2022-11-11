86bd19befa7baa487409ee621c4a5da214ab1a88
Ajay Singh <32979787+ajay87891@users.noreply.github.com>
2022-11-11 23:31:39 +0530

    refactor and change icon

--- src/App.js
+++ src/App.js
@@ -24,22 +24,22 @@ export default class App extends Component {
         <Router>
           <Navigation />
           <Routes>
-            <Route exact path="/business" element={<News key="business" pagesize={9} country="in" category="business" />} />
+            <Route exact path="/business" element={<News key="business" pagesize={this.props.pagesize} country="in" category="business" />} />
            
-            <Route exact path="/entertainment" element={<News key="entertainment" pagesize={9} country="in" category="entertainment" />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pagesize={this.props.pagesize} country="in" category="entertainment" />} />
             
             <Route exact path="/" element={
-              <News key="general" pagesize={9} country="in" category="general" />} />
+              <News key="general" pagesize={this.props.pagesize} country="in" category="general" />} />
             
             <Route exact path="/health" element={
-              <News key="health" pagesize={9} country="in" category="health" />} />
+              <News key="health" pagesize={this.props.pagesize} country="in" category="health" />} />
             
             <Route exact path="/science" element={
-              <News key="science" pagesize={9} country="in" category="science" />} />
+              <News key="science" pagesize={this.props.pagesize} country="in" category="science" />} />
             <Route exact path="/sports" element={
-              <News key="sports" pagesize={9} country="in" category="sports" />} />
+              <News key="sports" pagesize={this.props.pagesize} country="in" category="sports" />} />
             <Route exact path="/technology" element={
-              <News key="technology" pagesize={9} country="in" category="technology" />} />
+              <News key="technology" pagesize={this.props.pagesize} country="in" category="technology" />} />
           </Routes>
         </Router>
       </div>
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,144 +2,130 @@ import { Spinner } from "flowbite-react";
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
-
 export class News extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       articles: [],
       loading: false,
       page: 1,
-      totalResults: 0
+      totalResults: 0,
     };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} News`
   }
-
-  async componentDidMount() {
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0f03cebe013543198abee230ab815999&page=1&pageSize=${this.props.pagesize}`;
-      this.setState({loading: true})
+   capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  async UpdateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0f03cebe013543198abee230ab815999&page=${this.state.page}&pageSize=${this.props.pagesize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
+    console.log("upsated")
 
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false
+      loading: false,
     });
   }
-   handelnextclick = async ()=>{
-    if(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pagesize)){
 
-    }else{
-
-    
-    console.log("next")
+  async componentDidMount() {
+    this.UpdateNews();
+  }
+  handelnextclick = async () => {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pagesize)
+    ) {
+    } else {
+      this.setState({
+        page: this.state.page + 1,
+      });
+      this.UpdateNews();
+    }
+  };
+  handelprevclick = async () => {
     this.setState({
-      page: this.state.page+1
-    })
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0f03cebe013543198abee230ab815999&page=${this.state.page+1}&pageSize=${this.props.pagesize}`;
-    this.setState({loading: true})
-    let data = await fetch(url);
-    let parsedData = await data.json();
-
-    this.setState({
-      articles: parsedData.articles,
-      loading:false
+      page: this.state.page - 1,
     });
-  }
-  }
-  handelprevclick = async ()=>{
-    console.log("prev")
-    this.setState({
-      page: this.state.page-1
-    })
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0f03cebe013543198abee230ab815999&page=${this.state.page-1}&pageSize=${this.props.pagesize}`;
-      this.setState({loading: true})
-
-    let data = await fetch(url);
-    let parsedData = await data.json();
-
-    this.setState({
-      articles: parsedData.articles,
-      loading: false
-    });
-
-
-  }
-
+    this.UpdateNews();
+  };
 
   render() {
     return (
       <>
         <div className="flex items-center flex-col">
-          <h1 className="text-slate-800 text-6xl mb-4 font-serif dark:text-slate-300">
-            Today's Headlines
+          <h1 className="text-slate-800 text-2xl mb-4 font-serif dark:text-slate-300">
+            Today's Headlines From {this.capitalizeFirstLetter(this.props.category)}
           </h1>
-          {this.state.loading && <Spinner/>}
+          {this.state.loading && <Spinner />}
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 auto-cols-max ">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <NewsItem
-                key={element.url}
-                title={element.title}
-                description={element.description}
-                imgurl={element.urlToImage}
-                newsurl={element.url}
-                author = {element.author}
-                date = {element.publishedAt}
-              />
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <NewsItem
+                  key={element.url}
+                  title={element.title}
+                  description={element.description}
+                  imgurl={element.urlToImage}
+                  newsurl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                />
+              );
+            })}
         </div>
         <hr className="my-7 text-slate-800 dark:text-slate-500 shadow-md rounded mx-4"></hr>
         <div className="flex justify-between mx-20 sm:mx-32 md:mx-80 my-8">
-        <button
-              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:invisible "
-              onClick={this.handelprevclick}
-              disabled={this.state.page<=1}
+          <button
+            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:invisible "
+            onClick={this.handelprevclick}
+            disabled={this.state.page <= 1}
+          >
+            <svg
+              aria-hidden="true"
+              className="mr-2 -ml-1 w-4 h-4 rotate-180"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              
-              <svg
-                aria-hidden="true"
-                className="mr-2 -ml-1 w-4 h-4 rotate-180"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              Previous
-            </button>
-          
-           <button
-              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:invisible"
-              onClick={this.handelnextclick}
-              disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pagesize)}
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            Previous
+          </button>
+
+          <button
+            className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:invisible"
+            onClick={this.handelnextclick}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pagesize)
+            }
+          >
+            Next
+            <svg
+              aria-hidden="true"
+              className="ml-2 -mr-1 w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Next
-              <svg
-                aria-hidden="true"
-                className="ml-2 -mr-1 w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
         </div>
       </>
     );
