58f8871329d4caef83cf8f4e65ae77582c9f00c5
Ajay Singh <32979787+ajay87891@users.noreply.github.com>
2022-12-01 16:11:58 +0530

    api fixed

--- src/Components/News.js
+++ src/Components/News.js
@@ -25,8 +25,9 @@ const News = (props) => {
   }, []);
 
   const updateNews = async () => {
+    
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pagesize}`;
+    const url = `https://agreeable-raincoat-worm.cyclic.app/api/getnews?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pagesize}`;
     setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
@@ -39,7 +40,7 @@ const News = (props) => {
 
   const fetchMoreData = async () => {
     
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pagesize}`;
+    const url = `https://agreeable-raincoat-worm.cyclic.app/api/getnews?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pagesize}`;
     setpage(page + 1);
     setLoading(true);
     let data = await fetch(url);
